350ccc6da9a017cfa4bef4b08b5aacf78707a315
Antilko <76128978+Antilko@users.noreply.github.com>
2021-07-27 16:58:52 +0300

    final version

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,97 +1,72 @@
-const widget = function() {
+const TaskList = function() {
     let overlay = document.getElementById('overlay'),
-        buttonDone_1 = document.querySelectorAll('.widget__task-1_button'),
-        buttonDone_2 = document.querySelectorAll('.widget__task-2_button'),
-        buttonDone_3 = document.querySelectorAll('.widget__task-3_button'),
+        buttonFirstTask = document.querySelector('.widget__task-1_button'),
+        buttonSecondTask = document.querySelector('.widget__task-2_button'),
+        buttonThirdTask = document.querySelector('.widget__task-3_button'),
+        buttons = document.querySelector('.button-done'),
         closeButton = document.querySelectorAll('.modal__button-close');
 
-    buttonDone_1.forEach(function(item) {
-        
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
+    const taskFirst = function() {
+        let inputNumber = document.getElementById('input-number').value,
+            message = document.querySelector('.modal__message');
 
-            let inputNumber = document.getElementById('input-number').value,
-                message = document.querySelector('.modal__message');
+        if (inputNumber <= 7 || isNaN(inputNumber)) {
+            document.getElementById('input-number').value = '';
+            document.getElementById('input-number').placeholder='Укажите число больше 7';
+            document.getElementById('input-number').classList.add('placeholder_red');
+            return
+        } else {
+            message.innerHTML = `Привет`;
+        }
 
-            if (inputNumber <= 7 || isNaN(inputNumber)) {
-                document.getElementById('input-number').value = '';
-                document.getElementById('input-number').placeholder='Укажите число больше 7';
-                document.getElementById('input-number').classList.add('placeholder_red');
-                return
-            } else {
-                message.innerHTML = `Привет`;
-            }
+        modalPage(buttons);    
+    };
 
-            modalPage(item)
-        }); 
-    });
-
-    buttonDone_2.forEach(function(item) {
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
-
-            let inputName = document.getElementById('input-name').value,
+    const taskSecond = function() {
+        let inputName = document.getElementById('input-name').value,
                 message = document.querySelector('.modal__message'),
                 name = 'Вячеслав';
 
-            if (inputName.trim() == '') {
-                document.getElementById('input-name').value = '';
-                document.getElementById('input-name').placeholder='Необходимо ввести имя';
-                document.getElementById('input-name').classList.add('placeholder_red');
-                return
-            } else if (inputName.toLowerCase() !== name.toLowerCase()) {
-                message.innerHTML = `Нет такого имени`;
-            } else {
-                message.innerHTML = `Привет, ${inputName[0].toUpperCase() + inputName.slice(1).toLowerCase()}`;
-            }
+        if (inputName.trim() == '') {
+            document.getElementById('input-name').value = '';
+            document.getElementById('input-name').placeholder='Необходимо ввести имя';
+            document.getElementById('input-name').classList.add('placeholder_red');
+            return
+        } else if (inputName.toLowerCase() !== name.toLowerCase()) {
+            message.innerHTML = `Нет такого имени`;
+        } else {
+            message.innerHTML = `Привет, ${inputName[0].toUpperCase() + inputName.slice(1).toLowerCase()}`;
+        }
 
-            modalPage(item);
-        }); 
-    });
+        modalPage(buttons);    
+    };
 
-    buttonDone_3.forEach(function(item) {
+    const taskThird = function() {
+        let numberList = document.getElementById('input-numbers').value,
+            message = document.querySelector('.modal__message'),
+            inputArray = numberList.split(`,`).map(Number),
+            newArray = inputArray.filter(number => number % 3 === 0),
+            regexpNumber = /^[\d\s,-]*$/;
 
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
+        if (numberList.trim() == '' || regexpNumber.test(numberList) == false) {
+            document.getElementById("input-numbers").value = '';
+            document.getElementById('input-numbers').placeholder='Введите числа через запятую';
+            document.getElementById('input-numbers').classList.add('placeholder_red');
+            return
+        } else if (newArray.length === 0) {
+            message.innerHTML = `Совпадений нет`;
+        } else {
+            message.innerHTML = newArray.join(`, `);
+        }
 
-            let numberList = document.getElementById('input-numbers').value,
-                message = document.querySelector('.modal__message'),
-                inputArray = numberList.split(`,`).map(Number),
-                newArray = inputArray.filter(number => number % 3 === 0),
-                regexpNumber = /^[\d\s,-]*$/;
+        modalPage(buttons);    
+    };
 
-                if (numberList.trim() == '' || regexpNumber.test(numberList) == false) {
-                    document.getElementById("input-numbers").value = '';
-                    document.getElementById('input-numbers').placeholder='Введите числа через запятую';
-                    document.getElementById('input-numbers').classList.add('placeholder_red');
-                    return
-                } else if (newArray.length === 0) {
-                    message.innerHTML = `Совпадений нет`;
-                } else {
-                    message.innerHTML = newArray.join(`, `);
-                }
-
-            modalPage(item)
-        }); 
-    });
-
-    closeButton.forEach(function(item) {
-        item.addEventListener('click', function(event) {
-            event.preventDefault();
-            modalClose()
-        });
-    });
-
-    window.addEventListener('click', function(event) {
-        if (event.target == overlay) {
-            modalClose()
-        }    
-    });
-
-    const modalPage = function (item) {
-        let contentId = item.dataset.content,
+    const modalPage = function () {
+        let contentId = buttons.dataset.content,
             content = document.querySelector('#' + contentId),
             contents = document.querySelectorAll('.modal');
+            console.log(content)
 
         contents.forEach(function(content) {
             overlay.classList.add('active');
@@ -114,6 +89,34 @@ const widget = function() {
         document.getElementById("input-name").classList.remove('placeholder_red');
         document.getElementById("input-numbers").classList.remove('placeholder_red');
     }
+
+    buttonFirstTask.addEventListener('click', (e) => {
+        e.preventDefault();
+        taskFirst(buttons);
+    }); 
+
+    buttonSecondTask.addEventListener('click', (e) => {
+        e.preventDefault();
+        taskSecond(buttons);
+    }); 
+
+    buttonThirdTask.addEventListener('click', (e) => {
+        e.preventDefault();
+        taskThird(buttons);
+    }); 
+
+    closeButton.forEach(function(item) {
+        item.addEventListener('click', function(event) {
+            event.preventDefault();
+            modalClose()
+        });
+    });
+
+    window.addEventListener('click', function(event) {
+        if (event.target == overlay) {
+            modalClose()
+        }    
+    });
 }
 
-widget();
+TaskList();
